a36b352abcfead751c4ab828af3197344e0e492f
DESKTOP-819V2FI\Lenovo <272070464@qq.com>
2020-05-14 17:24:58 +0800

    no message

--- week05/toy-browser/client.js
+++ week05/toy-browser/client.js
@@ -204,7 +204,7 @@ void async function() {
 	let request = new Request({
 		method: "POST",
 		host: '127.0.0.1',
-		port: '8099',
+		port: 8099,
 		path: '/',
 		headers: {
 			["X-Foo2"]: "customed"
